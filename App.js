b8dd79d19d47e19bc4ae2ff22c1137021baa2294
sami-0301 <100032364+sami-0301@users.noreply.github.com>
2023-01-19 21:39:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { useState } from 'react';
+import Welcome from './Welcome/Welcome';
+
+function App() {
+  return (
+    <div >
+      
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
